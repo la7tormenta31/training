c9dd0c2f331b5bd497efafd5c8479098fe442baf
la7tormenta31 <88268421+la7tormenta31@users.noreply.github.com>
2021-08-21 16:31:33 +0900

    Add files via upload

---
+++ modalwindow.js
@@ -0,0 +1,13 @@
+ $('img').click(function() {
+        var imgSrc = $(this).attr('src');
+        $('.bigimg').children().attr('src',imgSrc);
+        $('.modal').fadeIn();
+        $('body,html').css('overflow-y', 'hidden');
+        return false
+      });
+
+$('.close-btn').click(function() {
+        $('.modal').fadeOut();
+        $('body,html').css('overflow-y', 'visible');
+        return false
+      });
